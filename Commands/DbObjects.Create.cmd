12bc97456fcc7de01c10b97f08ec0f2d28b463ef
Jani Giannoudis <jani@giannoudis.ch>
2023-07-15 10:42:25 +0200

    cmd tweak

--- Commands/DbObjects.Create.cmd
+++ Commands/DbObjects.Create.cmd
@@ -4,6 +4,5 @@ rem query tool
 set query=PayrollDbQuery
 if not "%PayrollDbQuery%" == "" set query=%PayrollDbQuery%
 
-@echo off
 echo query database create object script
 call %query% Query ../Database/Current/CreateObjectsDbScript.sql
