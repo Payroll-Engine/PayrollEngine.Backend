57e8be5065ece2bb28fb6bf4766a9fce8a5f2435
Jani Giannoudis <jani@giannoudis.ch>
2023-07-23 08:24:20 +0200

    added swagger.json build updated version to 0.5.0-230721-3

---
+++ Commands/DotNet.Swagger.Install.cmd
@@ -0,0 +1,4 @@
+@echo OFF
+echo Setup dotnet tool swaggger...
+dotnet tool install --version 6.5.0 Swashbuckle.AspNetCore.Cli
+pause
--- Commands/Swagger.Build.cmd
+++ Commands/Swagger.Build.cmd
@@ -1,9 +1,9 @@
 @echo off
 
 @echo Build swagger.json...
+rem requires the dotnet tool swagger
+rem dotnet tool install --version 6.5.0 Swashbuckle.AspNetCore.Cli
 @echo.
 pushd ..\Backend.Server
 dotnet swagger tofile --output ..\docs\swagger.json ..\Backend.Server\bin\Release\net7.0\PayrollEngine.Backend.Server.dll PayrollEngineBackendAPI
 popd
-
-pause
